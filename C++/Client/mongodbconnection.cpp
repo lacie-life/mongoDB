5c9f382afa1dabd1c5ed2b64cf7b978233bdc968
lacie-life <00sao00ios00@gmail.com>
2020-09-22 11:31:25 +0700

    created mongdbconnection class

---
+++ C++/Client/mongodbconnection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lifezero on 22/09/2020.
+//
+
+#include "mongodbconnection.h"
---
+++ C++/Client/mongodbconnection.h
@@ -0,0 +1,14 @@
+//
+// Created by lifezero on 22/09/2020.
+//
+
+#ifndef CLIENT_MONGODBCONNECTION_H
+#define CLIENT_MONGODBCONNECTION_H
+
+
+class mongodbconnection {
+
+};
+
+
+#endif //CLIENT_MONGODBCONNECTION_H
