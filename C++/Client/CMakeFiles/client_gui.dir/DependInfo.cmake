af86f8776028364c88d5be415913d7fc2be7d6ef
lacie-life <00sao00ios00@gmail.com>
2020-09-23 19:19:00 +0700

    update Client View

---
+++ C++/Client/CMakeFiles/client_gui.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lifezero/Github/mongoDB/C++/Client/main.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/main.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/mainwindow.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/mainwindow.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/moc_mainwindow.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/moc_mainwindow.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/moc_mongodbconnection.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/moc_mongodbconnection.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/moc_mqttconnection.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/moc_mqttconnection.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/mongodbconnection.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/mongodbconnection.cpp.o"
+  "/home/lifezero/Github/mongoDB/C++/Client/mqttconnection.cpp" "/home/lifezero/Github/mongoDB/C++/Client/CMakeFiles/client_gui.dir/mqttconnection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/mavsdk"
+  "/usr/local/include/bsoncxx/v_noabi"
+  "/usr/local/include/mongocxx/v_noabi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ C++/Client/CMakeFiles/client_gui.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_gui.dir/main.cpp.o"
+  "CMakeFiles/client_gui.dir/mainwindow.cpp.o"
+  "CMakeFiles/client_gui.dir/moc_mainwindow.cpp.o"
+  "CMakeFiles/client_gui.dir/moc_mongodbconnection.cpp.o"
+  "CMakeFiles/client_gui.dir/moc_mqttconnection.cpp.o"
+  "CMakeFiles/client_gui.dir/mongodbconnection.cpp.o"
+  "CMakeFiles/client_gui.dir/mqttconnection.cpp.o"
+  "client_gui"
+  "client_gui.pdb"
+  "moc_mainwindow.cpp"
+  "moc_mongodbconnection.cpp"
+  "moc_mqttconnection.cpp"
+  "ui_mainwindow.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
